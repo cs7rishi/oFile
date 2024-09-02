1ccd0225b9bc167c2a76ed34fbbe694bba8db377
Rishi Singh <rishi.singh@maxlifeinsurance.com>
2024-09-02 23:46:37 +0530

    initalized basic spring boot project with dependency

---
+++ oFile_backend/src/main/java/com/cs7rishi/oFile/OFileApplication.java
@@ -0,0 +1,13 @@
+package com.cs7rishi.oFile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OFileApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OFileApplication.class, args);
+	}
+
+}
