7dc3fa333414cce95b99476cda5fd2051cc2e67b
Rishi Singh <cs24rishi@gmail.com>
2024-10-14 03:21:12 +0530

    fixed sign in component with colors

--- oFile_frontend/src/components/SignIn.js
+++ oFile_frontend/src/components/SignIn.js
@@ -1,6 +1,7 @@
 import { useNavigate } from "react-router-dom";
 import { URLConstants, Constants } from "../constants";
 import { setStorage, triggerNotification } from "../utils";
+import toast from "react-hot-toast"
 
 export const SignIn = () => {
   const navigate = useNavigate();
@@ -25,32 +26,31 @@ export const SignIn = () => {
         const data = await response.json();
         setStorage(Constants.AUTHORIZATION, response.headers.get(Constants.AUTHORIZATION))
         triggerNotification(data.response)
-        navigate("/dashboard")
+        navigate("/dashboard/downloads")
       }
-
     } catch (error) {
-      console.log(error)
+      toast.error("Something went Wrong!");
     }
   }
 
   return (
-    <div className="login-container w-full">
-      <form className="w-1/3 mx-auto" onSubmit={handleSubmit}>
+    <div className="login-container flex items-center w-full h-full bg-raisin-black">
+      <form className="w-1/3 mx-auto max-h-max" onSubmit={handleSubmit}>
         <div className="mb-5">
-          <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
-          <input name="email" type="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@flowbite.com" required />
+          <label htmlFor="email" className="block mb-2 text-sm font-medium text-white">Your email</label>
+          <input name="email" type="email" id="email" className="bg-alice-blue border border-gray-300 text-gray-900 text-sm rounded-lg block w-full p-2.5" placeholder="Email" required />
         </div>
         <div className="mb-5">
-          <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your password</label>
-          <input name="password" type="password" id="password" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required />
+          <label htmlFor="password" className="block mb-2 text-sm font-medium text-white">Your password</label>
+          <input name="password" type="password" id="password" className="bg-alice-blue border border-gray-300 text-gray-900 text-sm rounded-lg block w-full p-2.5" placeholder="Password" required />
         </div>
-        <div className="flex items-start mb-5">
+        {/* <div className="flex items-start mb-5">
           <div className="flex items-center h-5">
             <input id="remember" type="checkbox" value="" className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-blue-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800" required />
           </div>
           <label htmlFor="remember" className="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300">Remember me</label>
-        </div>
-        <button type="submit" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit</button>
+        </div> */}
+        <button type="submit" className="text-white bg-picton-blue hover:bg-picton-blue-800 focus:ring-4 focus:outline-none focus:ring-picton-blue font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center">Submit</button>
       </form>
     </div>
   )
