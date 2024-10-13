d4f783423ef8ef14c1d915113edd69761387c619
Rishi Singh <cs24rishi@gmail.com>
2024-10-14 02:24:31 +0530

    fixed colors

--- oFile_frontend/tailwind.config.js
+++ oFile_frontend/tailwind.config.js
@@ -1,7 +1,11 @@
 module.exports = {
     content: ["./src/**/*.{html,js}"],
     theme: {
-      extend: {},
+      extend: {
+        colors:{
+          'picton-blue': '#00abe4',
+        }
+      },
     },
     plugins: [],
   }
