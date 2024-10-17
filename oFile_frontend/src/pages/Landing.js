30c95b09063d32c1db1ddc4c80fa70f6e9e71e72
Rishi Singh <cs24rishi@gmail.com>
2024-10-18 02:28:32 +0530

    changes in landing page

--- oFile_frontend/src/pages/Landing.js
+++ oFile_frontend/src/pages/Landing.js
@@ -5,7 +5,7 @@ export const Landing = () => {
       <section class="h-full flex items-center object-center bg-white dark:bg-gray-900">
         <div class="py-8 px-4 mx-auto max-w-screen-xl text-center lg:py-16">
           <h1 class="mb-4 text-4xl font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-6xl dark:text-white">We speed up file download</h1>
-          <p class="mb-8 text-lg font-normal text-gray-500 lg:text-xl sm:px-16 lg:px-48 dark:text-gray-400">Here at SnapFile we focus on slow file download, download them concurrently and provide you faster download, or move to cloud of your choice. </p>
+          <p class="mb-8 text-lg font-normal text-gray-500 lg:text-xl sm:px-16 lg:px-48 dark:text-gray-400">Here at Ofile we focus on slow file download, download them concurrently and provide you faster download, or move to cloud of your choice. </p>
           <div class="flex flex-col space-y-4 sm:flex-row sm:justify-center sm:space-y-0">
             <Link to="/login" class="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-white rounded-lg bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-900">
               Sign In
